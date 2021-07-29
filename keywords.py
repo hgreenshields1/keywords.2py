c4a4944be1161c59dfe2087f9633fe22c3834b31
hope <hope.greenshields.2018@uni.strath.ac.uk>
2021-07-29 10:49:17 +0100

    29/07/2021

--- keywords.py
+++ keywords.py
@@ -3,27 +3,13 @@ import string
 import re
 from spellchecker import SpellChecker
 from textblob import TextBlob
+from autocorrect import Speller
 
-keywords = ["ML",
-            "datasets",
-            "the dataset",
-            "Machine learning",
-            "the field",
-            "computer science",
-            "automated systems",
-            "expert-written rules",
-            "the key technologies",
-            "the current fourth industrial revolution",
-            "the ML market",
-            "$6.9B",
-            "a 43.8% compound annual growth rate",
-            "The sub",
-            "fields",
-            "various ways",
-            "A divide",
-            "classical' machine learning techniques",
-            "'deep learning",
-            "(DL"]
+keywords = ["our energy supply", "smart grids", "Smart grids", "a key enabler", "a more sustainable power system",
+            "real-time data gathering", "the intelligent control", "The aim", "a smart grid", "Yu", "Xue",
+            "the behaviours", "actions", "all the stakeholders", "the energy supply chain",
+            "sustainable, economic and secure electric energy", "economical and environmentally sustainable use",
+            "the inception", "smart meter systems", "peer"]
 
 
 def NoPunctuation(words):
@@ -52,13 +38,46 @@ def NoThe(words):
     for word in lower:
         x = re.findall("the +", word)
         if x:
-            noThe = word.replace("the", '')
+            noThe = word.replace("the ", '')
             noThe = noThe.strip()
             withoutThe.append(noThe)
         else:
             withoutThe.append(word)
-
     return withoutThe
+
+
+def removeA(words):
+    withoutA = []
+    lower = LowerCase(words)
+    for word in lower:
+        x = re.findall("a +", word)
+        y = re.findall("^a ", word)
+        if y:
+            noA = word.replace("a ", '')
+            noA = noA.strip()
+            withoutA.append(noA)
+        elif x:
+            noA = word.replace(" a ", '')
+            noA = noA.strip()
+            withoutA.append(noA)
+        else:
+            withoutA.append(word)
+    return withoutA
+
+
+def removeOf(words):
+    withoutOf = []
+    lower = LowerCase(words)
+    for word in lower:
+        x = re.findall("of +", word)
+        if x:
+            noOf = word.replace("of ", '')
+            noOf = noOf.strip()
+            withoutOf.append(noOf)
+        else:
+            withoutOf.append(word)
+
+    return withoutOf
 
 
 def splitAnd(words):
@@ -78,19 +97,43 @@ def splitAnd(words):
 
 def autoCorrect(words):
     # spell = SpellChecker()
+    check = Speller(lang='en')
     corrected = []
     for word in words:
-        correct = TextBlob(word)
-        correct = str(correct.correct())
+        correct = check(word)
+        # correct = TextBlob(word)
+        # correct = str(correct.correct())
         corrected.append(correct)
 
     return corrected
 
 
+def removeSomeNumbers(words):
+    noNumbers = []
+    for word in words:
+        split = word.split(" ")
+        backTogether = ""
+        for entity in split:
+
+            chars = set('$%£€')
+            if any((c in chars) for c in entity):
+                continue
+            else:
+                backTogether = backTogether + " " + entity
+                backTogether = backTogether.lstrip()
+        if backTogether != "":
+            noNumbers.append(backTogether)
+
+    return noNumbers
+
+
 def runAll():
     lower = LowerCase(keywords)
-    noThe = NoThe(lower)
-    splitUp = splitAnd(noThe)
+    noNum = removeSomeNumbers(lower)
+    noThe = NoThe(noNum)
+    noA = removeA(noThe)
+    noOf = removeOf(noA)
+    splitUp = splitAnd(noOf)
     noPunct = NoPunctuation(splitUp)
     correct = autoCorrect(noPunct)
 
